153ac0b1d9156f46b01af813b9fa443d8d27cb27
Kostas Kokkoros <mavroprovato@gmail.com>
2022-02-25 01:10:36 +0200

    Change pagination to match the stachexchange API schema

--- stackexchange/pagination.py
+++ stackexchange/pagination.py
@@ -1,4 +1,8 @@
+import collections
+import typing
+
 from rest_framework import pagination
+from rest_framework.response import Response
 
 
 class Pagination(pagination.PageNumberPagination):
@@ -7,3 +11,14 @@ class Pagination(pagination.PageNumberPagination):
     page_size = 30
     page_size_query_param = 'pagesize'
     max_page_size = 100
+
+    def get_paginated_response(self, data: typing.Iterable) -> Response:
+        """Return the paginated response.
+
+        :param data: The data.
+        :return: The paginated response.
+        """
+        return Response(collections.OrderedDict([
+            ('items', data),
+            ('has_more', self.page.has_next()),
+        ]))
