1023798b5568f2d01d510adb4b1bc4665b284d3e
Kostas Kokkoros <mavroprovato@gmail.com>
2022-02-25 00:21:20 +0200

    Add the date range filter for the comments view sets

--- stackexchange/views/comments.py
+++ stackexchange/views/comments.py
@@ -1,19 +1,19 @@
 from drf_spectacular.utils import extend_schema_view, extend_schema
 
 from stackexchange import enums, filters, models, serializers
-from .base import BaseViewSet
+from .base import BaseViewSet, DateFilteringViewSetMixin
 
 
 @extend_schema_view(
     list=extend_schema(summary='Get all comments on the site'),
     retrieve=extend_schema(summary='Gets the comment identified by id'),
 )
-class CommentViewSet(BaseViewSet):
+class CommentViewSet(BaseViewSet, DateFilteringViewSetMixin):
     """The answers view set
     """
     queryset = models.Comment.objects.select_related('post', 'user')
     serializer_class = serializers.CommentSerializer
-    filter_backends = (filters.OrderingFilter, )
+    filter_backends = (filters.OrderingFilter, filters.DateRangeFilter)
 
     @property
     def ordering_fields(self):
@@ -25,3 +25,11 @@ class CommentViewSet(BaseViewSet):
             ('creation', enums.OrderingDirection.DESC.value, 'creation_date'),
             ('votes', enums.OrderingDirection.DESC.value, 'score')
         )
+
+    @property
+    def date_field(self) -> str:
+        """Return the field used for date filtering.
+
+        :return: The field used for date filtering.
+        """
+        return 'creation_date'
