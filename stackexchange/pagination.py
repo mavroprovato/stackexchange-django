c9be6f77b30e20ab23c59b8154e899e92b4a0c2f
Kostas Kokkoros <mavroprovato@gmail.com>
2022-02-26 04:53:41 +0200

    Fix pagination

--- stackexchange/pagination.py
+++ stackexchange/pagination.py
@@ -5,7 +5,6 @@ import collections.abc
 import typing
 
 from django.core.paginator import InvalidPage, PageNotAnInteger, EmptyPage
-from django.db.models import QuerySet
 from django.views import View
 from django.utils.translation import gettext_lazy as _
 from rest_framework import pagination
@@ -17,20 +16,19 @@ from rest_framework.response import Response
 class Page(collections.abc.Sequence):
     """The default page implementation.
     """
-    def __init__(self, object_list: typing.Iterable, number: int, paginator) -> None:
+    def __init__(self, object_list: typing.Collection, page_size: int, paginator) -> None:
         """Create the page.
 
         :param object_list: The object list.
-        :param number: The page number.
+        :param page_size: The page size.
         :param paginator: The paginator.
         """
+        self.object_list = object_list
         # The object_list is converted to a list so that if it was a QuerySet it won't be a database hit per
         # __getitem__.
-        if isinstance(object_list, list):
-            self.object_list = object_list
-        else:
-            self.object_list = list(object_list)
-        self.number = number
+        if not isinstance(self.object_list, list):
+            self.object_list = list(self.object_list)
+        self.page_size = page_size
         self.paginator = paginator
 
     def __getitem__(self, index: typing.Union[int, slice]) -> object:
@@ -49,20 +47,20 @@ class Page(collections.abc.Sequence):
 
         :return: The actual number of items for the page.
         """
-        return len(self.object_list[:self.paginator.page_size])
+        return len(self.object_list[:self.page_size])
 
     def has_next(self) -> bool:
         """Return true if there is a next page.
 
         :return: true if there is a next page.
         """
-        return self.number > self.__len__()
+        return len(self.object_list) > self.page_size
 
 
 class Paginator:
     """The default paginator.
     """
-    def __init__(self, queryset: QuerySet, page_size: int) -> None:
+    def __init__(self, queryset: typing.Collection, page_size: int) -> None:
         """Create the paginator.
 
         :param queryset: The queryset to paginate.
@@ -81,7 +79,7 @@ class Paginator:
         bottom = (number - 1) * self.page_size
         top = bottom + self.page_size
 
-        return Page(self.queryset[bottom:top+1], number, self)
+        return Page(self.queryset[bottom:top+1], self.page_size, self)
 
     @staticmethod
     def validate_number(number: str) -> int:
@@ -94,8 +92,8 @@ class Paginator:
             if isinstance(number, float) and not number.is_integer():
                 raise ValueError
             number = int(number)
-        except (TypeError, ValueError) as exeption:
-            raise PageNotAnInteger(_('That page number is not an integer')) from exeption
+        except (TypeError, ValueError) as exception:
+            raise PageNotAnInteger(_('That page number is not an integer')) from exception
         if number < 1:
             raise EmptyPage(_('That page number is less than 1'))
 
@@ -115,7 +113,7 @@ class Pagination(pagination.PageNumberPagination):
         """
         self.page = None
 
-    def paginate_queryset(self, queryset: QuerySet, request: Request, view: View = None) -> typing.Optional[list]:
+    def paginate_queryset(self, queryset: typing.Collection, request: Request, view: View = None) -> typing.Collection:
         """Paginate a queryset if required, either returning a page object, or `None` if pagination is not configured
         for this view.
 
@@ -126,7 +124,7 @@ class Pagination(pagination.PageNumberPagination):
         """
         page_size = self.get_page_size(request)
         if not page_size:
-            return None
+            return queryset
         paginator = self.django_paginator_class(queryset, page_size)
         page_number = self.get_page_number(request, paginator)
 
