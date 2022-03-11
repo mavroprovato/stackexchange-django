a780bf9b52131890eb8b6849b50e52e24bceb36e
Kostas Kokkoros <mavroprovato@gmail.com>
2022-03-12 00:44:37 +0200

    Add factory boy, and implement user factory

---
+++ stackexchange/tests/factories/user.py
@@ -0,0 +1,24 @@
+"""The user factory
+"""
+import factory
+
+from stackexchange import models
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """The user factory
+    """
+    class Meta:
+        model = models.User
+        django_get_or_create = ('username', )
+
+    username = factory.Faker('user_name')
+    email = factory.LazyAttribute(lambda obj: '%s@example.com' % obj.username)
+    display_name = factory.Faker('name')
+    website_url = factory.Faker('url')
+    location = factory.Faker('city')
+    about = factory.Faker('sentence')
+    reputation = factory.Faker('pyint')
+    views = factory.Faker('pyint')
+    up_votes = factory.Faker('pyint')
+    down_votes = factory.Faker('pyint')
--- stackexchange/tests/test_api/test_users.py
+++ stackexchange/tests/test_api/test_users.py
@@ -1,15 +1,36 @@
 """Users API testing
 """
+import dateutil.parser
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from .. import factories
+from stackexchange import models
 
 
 class UserTests(APITestCase):
     """Users view set tests
     """
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the test data.
+        """
+        factories.UserFactory.create_batch(size=100)
+
     def test_list(self):
         """Test users list endpoint
         """
+        # Test that the list endpoint returns successfully
         response = self.client.get(reverse('user-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that the user information returned is correct
+        for row in response.json()['items']:
+            user = models.User.objects.get(pk=row['user_id'])
+            self.assertEqual(row['is_employee'], user.is_employee)
+            self.assertEqual(row['reputation'], user.reputation)
+            self.assertEqual(dateutil.parser.parse(row['creation_date']), user.creation_date)
+            self.assertEqual(row['location'], user.location)
+            self.assertEqual(row['website_url'], user.website_url)
+            self.assertEqual(row['display_name'], user.display_name)
