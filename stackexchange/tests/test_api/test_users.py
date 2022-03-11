ae461d5460d7ccababe2e3893b8968cecb568eda
Kostas Kokkoros <mavroprovato@gmail.com>
2022-03-12 01:53:54 +0200

    Test the user privileges endpoint

--- stackexchange/tests/test_api/test_users.py
+++ stackexchange/tests/test_api/test_users.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from .. import factories
-from stackexchange import models
+from stackexchange import enums, models
 
 
 class UserTests(APITestCase):
@@ -103,3 +103,21 @@ class UserTests(APITestCase):
         response = self.client.get(reverse('user-detail', kwargs={'pk': ';'.join(str(user.pk) for user in users)}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertSetEqual({row['user_id'] for row in response.json()['items']}, {user.pk for user in users})
+
+    def test_privileges(self):
+        """Test the user privileges endpoint.
+        """
+        user = random.sample(list(models.User.objects.all()), 1)[0]
+        response = self.client.get(reverse('user-privileges', kwargs={'pk': user.pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Assert that the privileges are returned by ascending reputation
+        self.assertListEqual([row['reputation'] for row in response.json()['items']],
+                             sorted([row['reputation'] for row in response.json()['items']]))
+        # Assert that the user has the correct privileges based on the reputation
+        user_privileges = [privilege for privilege in enums.Privilege if privilege.reputation <= user.reputation]
+        self.assertEqual(len(user_privileges), len(response.json()['items']))
+        # Assert the privilege descriptions and short descriptions are correct
+        self.assertListEqual([privilege.description for privilege in user_privileges],
+                             [row['description'] for row in response.json()['items']])
+        self.assertListEqual([privilege.name.replace('_', ' ').capitalize() for privilege in user_privileges],
+                             [row['short_description'] for row in response.json()['items']])
