511e2ae993ca99377a24eb05ab3c59bf7c3bb4e8
Kostas Kokkoros <mavroprovato@gmail.com>
2022-03-12 04:25:54 +0200

    Test sorting of posts

--- stackexchange/tests/test_api/test_posts.py
+++ stackexchange/tests/test_api/test_posts.py
@@ -33,3 +33,42 @@ class PostTests(APITestCase):
             self.assertEqual(dateutil.parser.parse(row['creation_date']), post.creation_date)
             self.assertEqual(row['post_type'], enums.PostType(post.type).name.lower())
             self.assertEqual(row['content_license'], enums.ContentLicense[post.content_license].name)
+
+    def test_list_sort_by_activity(self):
+        """Test the post list sorted by activity date.
+        """
+        response = self.client.get(reverse('post-list'), data={'sort': 'activity', 'order': 'asc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        reputations = [row['last_activity_date'] for row in response.json()['items']]
+        self.assertListEqual(reputations, sorted(reputations))
+
+        response = self.client.get(reverse('post-list'), data={'sort': 'activity', 'order': 'desc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        reputations = [row['last_activity_date'] for row in response.json()['items']]
+        self.assertListEqual(reputations, sorted(reputations, reverse=True))
+
+    def test_list_sort_by_creation(self):
+        """Test the post list sorted by creation date.
+        """
+        response = self.client.get(reverse('post-list'), data={'sort': 'creation', 'order': 'asc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        reputations = [row['creation_date'] for row in response.json()['items']]
+        self.assertListEqual(reputations, sorted(reputations))
+
+        response = self.client.get(reverse('post-list'), data={'sort': 'creation', 'order': 'desc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        reputations = [row['creation_date'] for row in response.json()['items']]
+        self.assertListEqual(reputations, sorted(reputations, reverse=True))
+
+    def test_list_sort_by_votes(self):
+        """Test the post list sorted by votes.
+        """
+        response = self.client.get(reverse('post-list'), data={'sort': 'votes', 'order': 'asc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        reputations = [row['score'] for row in response.json()['items']]
+        self.assertListEqual(reputations, sorted(reputations))
+
+        response = self.client.get(reverse('post-list'), data={'sort': 'votes', 'order': 'desc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        reputations = [row['score'] for row in response.json()['items']]
+        self.assertListEqual(reputations, sorted(reputations, reverse=True))
