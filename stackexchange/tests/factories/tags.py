f4f29aceb4dce2c8de927c56ab4fab0e263d3270
Kostas Kokkoros <mavroprovato@gmail.com>
2022-03-19 23:11:57 +0200

    Add tag list tests

---
+++ stackexchange/tests/factories/tags.py
@@ -0,0 +1,16 @@
+"""The tags factory
+"""
+import factory
+
+from stackexchange import models
+
+
+class TagFactory(factory.django.DjangoModelFactory):
+    """The tags factory
+    """
+    class Meta:
+        model = models.Tag
+        django_get_or_create = ('name', )
+
+    name = factory.Faker('slug')
+    award_count = factory.Faker('pyint')
---
+++ stackexchange/tests/test_api/tags/list.py
@@ -0,0 +1,28 @@
+"""Tag view set list testing
+"""
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from stackexchange import models
+from stackexchange.tests import factories
+
+
+class TagListTests(APITestCase):
+    """Tag view set list tests
+    """
+    @classmethod
+    def setUpTestData(cls):
+        factories.UserFactory.create_batch(size=10)
+
+    def test(self):
+        """Test tag list endpoint
+        """
+        # Test that the list endpoint returns successfully
+        response = self.client.get(reverse('tag-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that the tag information returned is correct
+        for row in response.json()['items']:
+            tag = models.Tag.objects.get(name=row['name'])
+            self.assertEqual(row['award_count'], tag.count)
