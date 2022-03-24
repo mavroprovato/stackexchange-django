3661fa4fe1813ecd7c9f242d2311ded974e31795
Kostas Kokkoros <mavroprovato@gmail.com>
2022-03-24 20:37:00 +0200

    User badges retrieve endpoint tests

--- stackexchange/tests/test_api/badges/retrieve.py
+++ stackexchange/tests/test_api/badges/retrieve.py
@@ -1,16 +1,16 @@
 """Badges API retrieve testing
 """
 import random
+import unittest
 
 from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
 
-from stackexchange import models
+from stackexchange import enums, models
 from stackexchange.tests import factories
+from .base import BaseBadgeTestCase
 
 
-class BadgeRetrieveTests(APITestCase):
+class BadgeRetrieveTests(BaseBadgeTestCase):
     """Badges retrieve tests
     """
     @classmethod
@@ -18,17 +18,115 @@ class BadgeRetrieveTests(APITestCase):
         factories.BadgeFactory.create_batch(size=100)
 
     def test(self):
-        """Test the badges detail endpoint.
+        """Test the badges retrieve endpoint.
         """
         badge = random.sample(list(models.Badge.objects.all()), 1)[0]
         response = self.client.get(reverse('badge-detail', kwargs={'pk': badge.pk}))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json()['items'][0]['badge_id'], badge.pk)
+        self.assert_items_equal(response)
 
     def test_multiple(self):
-        """Test the badges detail endpoint for multiple ids.
+        """Test the badges retrieve endpoint for multiple ids.
         """
         badges = random.sample(list(models.Badge.objects.all()), 3)
         response = self.client.get(reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertSetEqual({row['badge_id'] for row in response.json()['items']}, {badge.pk for badge in badges})
+        self.assert_items_equal(response)
+
+    def test_sort_by_rank(self):
+        """Test the badges retrieve endpoint sorted by badge rank.
+        """
+        badges = random.sample(list(models.Badge.objects.all()), 3)
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'rank', 'order': 'asc'}
+        )
+        self.assert_sorted(response, 'rank', transform=lambda x: enums.BadgeClass[x.upper()].value)
+
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'rank', 'order': 'desc'}
+        )
+        self.assert_sorted(response, 'rank', transform=lambda x: enums.BadgeClass[x.upper()].value, reverse=True)
+
+    @unittest.skip("Postgres and python sorting algorithms differ")
+    def test_sort_by_name(self):
+        """Test the badges retrieve endpoint sorted by badge name.
+        """
+        badges = random.sample(list(models.Badge.objects.all()), 3)
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'name', 'order': 'asc'}
+        )
+        self.assert_sorted(response, 'name')
+
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'name', 'order': 'desc'}
+        )
+        self.assert_sorted(response, 'name', reverse=True)
+
+    def test_sort_by_type(self):
+        """Test the badges retrieve endpoint sorted by badge type.
+        """
+        badges = random.sample(list(models.Badge.objects.all()), 3)
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'type', 'order': 'asc'}
+        )
+        self.assert_sorted(response, 'badge_type', transform=lambda x: enums.BadgeType[x.upper()].value)
+
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'type', 'order': 'desc'}
+        )
+        self.assert_sorted(response, 'badge_type', transform=lambda x: enums.BadgeType[x.upper()].value, reverse=True)
+
+    def test_range_by_rank(self):
+        """Test the badges retrieve endpoint range by badge rank.
+        """
+        badges = random.sample(list(models.Badge.objects.all()), 3)
+        min_value = enums.BadgeClass.SILVER
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'rank', 'min': min_value.name.lower()}
+        )
+        self.assert_range(response, 'rank', transform=lambda x: enums.BadgeClass[x.upper()].value,
+                          min_value=min_value.value)
+        max_value = enums.BadgeClass.SILVER
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'rank', 'max': max_value.name.lower()}
+        )
+        self.assert_range(response, 'rank', transform=lambda x: enums.BadgeClass[x.upper()].value,
+                          max_value=max_value.value)
+
+    @unittest.skip("Postgres and python sorting algorithms differ")
+    def test_range_by_name(self):
+        """Test the badges retrieve endpoint range by badge name.
+        """
+        badges = random.sample(list(models.Badge.objects.all()), 3)
+        min_value = 'b'
+        max_value = 'x'
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'name', 'min': min_value, 'max': max_value}
+        )
+        self.assert_range(response, 'name', min_value, max_value)
+
+    def test_range_by_type(self):
+        """Test the badges retrieve endpoint range by badge type.
+        """
+        badges = random.sample(list(models.Badge.objects.all()), 3)
+        min_value = enums.BadgeType.TAG_BASED
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'type', 'min': min_value.name.lower()}
+        )
+        self.assert_range(response, 'badge_type', transform=lambda x: enums.BadgeType[x.upper()].value,
+                          min_value=min_value.value)
+        max_value = enums.BadgeType.NAMED
+        response = self.client.get(
+            reverse('badge-detail', kwargs={'pk': ';'.join(str(badge.pk) for badge in badges)}),
+            data={'sort': 'type', 'max': max_value.name.lower()}
+        )
+        self.assert_range(response, 'badge_type', transform=lambda x: enums.BadgeType[x.upper()].value,
+                          max_value=max_value.value)
