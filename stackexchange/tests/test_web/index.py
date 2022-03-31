33dd1ffbd50903165d41fd3e3ddfa1f6d048426b
Kostas Kokkoros <mavroprovato@gmail.com>
2022-03-31 22:44:57 +0300

    Add web tests

---
+++ stackexchange/tests/test_web/index.py
@@ -0,0 +1,16 @@
+"""Index view tests
+"""
+import http
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTests(TestCase):
+    """Index view tests
+    """
+    def test(self):
+        """Test the index view
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
