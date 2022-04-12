f8e456e94beab8929e2447c4cd2f30f18728ea17
Kostas Kokkoros <mavroprovato@gmail.com>
2022-04-12 03:03:20 +0300

    Add documentation

--- stackexchange/tests/test_api/badges/list.py
+++ stackexchange/tests/test_api/badges/list.py
@@ -14,6 +14,8 @@ class BadgeListTests(BadgeWithAwardCountTestCase):
     """
     @classmethod
     def setUpTestData(cls):
+        """Set up the test data.
+        """
         users = factories.UserFactory.create_batch(size=10)
         badges = factories.BadgeFactory.create_batch(size=25)
         for _ in range(1000):
