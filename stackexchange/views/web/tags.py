48a7d98c8bda28f2fe060072ed6ee659f5a823a7
Kostas Kokkoros <mavroprovato@gmail.com>
2022-04-14 07:14:12 +0300

    Add tag question count

--- stackexchange/views/web/tags.py
+++ stackexchange/views/web/tags.py
@@ -1,6 +1,6 @@
 """Web tag views
 """
-from django.db.models import QuerySet, Subquery, OuterRef
+from django.db.models import QuerySet, Subquery, OuterRef, Count
 from django.views.generic import ListView
 
 from stackexchange import models
@@ -19,7 +19,10 @@ class TagView(ListView):
         :return: The queryset for the view.
         """
         return models.Tag.objects.annotate(
-            exprert=Subquery(models.Post.objects.filter(pk=OuterRef('excerpt_id')).values('body')[:1])
+            exprert=Subquery(models.Post.objects.filter(pk=OuterRef('excerpt_id')).values('body')[:1]),
+            question_count=models.Post.objects.filter(tags=OuterRef('pk')).values('tags').annotate(
+                question_count=Count('*')
+            ).values('question_count')
         ).order_by('-award_count')
 
     def get_context_data(self, **kwargs) -> dict:
