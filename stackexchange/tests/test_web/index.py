2f6f7d87cc3b0ce7748993a505836ab6224b25a9
Kostas Kokkoros <mavroprovato@gmail.com>
2022-04-16 03:33:28 +0300

    Add some test data for web tests

--- stackexchange/tests/test_web/index.py
+++ stackexchange/tests/test_web/index.py
@@ -5,10 +5,20 @@ import http
 from django.test import TestCase
 from django.urls import reverse
 
+from .. import factories
+
 
 class IndexViewTests(TestCase):
     """Index view tests
     """
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the test data.
+        """
+        users = factories.UserFactory.create_batch(size=100)
+        for user in users:
+            factories.QuestionFactory.create_batch(size=3, owner=user)
+
     def test(self):
         """Test the index view
         """
