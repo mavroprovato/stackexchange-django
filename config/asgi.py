286b7f21781bd4935e8b799e986270442b01e293
Kostas Kokkoros <mavroprovato@gmail.com>
2025-04-26 01:52:40 +0300

    Update to Django 5.2

--- config/asgi.py
+++ config/asgi.py
@@ -1,10 +1,10 @@
 """
-ASGI config for config project.
+ASGI config for stackexchange-django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for config project.
+WSGI config for stackexchange-django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
